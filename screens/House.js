665dabaf9f58a1cf9643542b6ed706ef31427e48
Keli Booster <kbooster17@gmail.com>
2021-11-09 04:48:23 +0000

    add screens

---
+++ screens/House.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+export class House extends Component {
+  render() {
+    return (
+      <View>
+        <Text> This is the house component </Text>
+      </View>
+    );
+  }
+}
+
+export default House;
---
+++ screens/Users.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+export class Users extends Component {
+  render() {
+    return (
+      <View>
+        <Text> This is the users component </Text>
+      </View>
+    );
+  }
+}
+
+export default Users;
