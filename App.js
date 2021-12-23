ec73fd3a5ff297a3c8e10f22f4687592e0df3304
Keli Booster <kbooster17@gmail.com>
2021-12-23 22:54:25 +0000

    add bredhcers

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 import React from "react";
 
 import HomeNavigation from "./navigations/HomeNavigation";
 
+const rootReducer = combineReducers({
+  Houses: houseReducer,
+});
+
+const store = createStore(rootReducer);
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <HomeNavigation />
-      <StatusBar />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <HomeNavigation />
+        <StatusBar />
+      </NavigationContainer>
+    </Provider>
   );
 }
